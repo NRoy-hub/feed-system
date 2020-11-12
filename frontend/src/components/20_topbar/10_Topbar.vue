635ed1afc9982c8dd282862ce6374eabd276b13a
NRoy-hub <code0zhem@gmail.com>
2020-11-12 09:17:38 +0900

    frontend > write style code for Topbar

--- frontend/src/components/20_topbar/10_Topbar.vue
+++ frontend/src/components/20_topbar/10_Topbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="topbar">
-    <span>[2020.11.13] 김태혁</span>
+    <div class="info">[2020.11.13] 김태혁</div>
   </header>
 </template>
 
@@ -12,7 +12,15 @@
 
 <style lang="scss" scoped>
   .topbar{
+    width: 100%;
     height: 45px;
-    color: $unused;
+    padding: 0 15px;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.07);
+
+    .info{
+      padding-top: 16px;
+      line-height: 25px;
+      color: #212529;
+    }
   }
 </style>
